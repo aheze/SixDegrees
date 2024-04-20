5518838a48f905803a3f29aedcafc17f57017c44
Andrew Zheng <aheze@getfind.app>
2024-04-19 19:15:57 -0700

    Initial Commit

---
+++ LookUp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LookUp
+//  
+//  Created by Andrew Zheng (github.com/aheze) on 4/19/24.
+//  Copyright © 2024 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LookUp/LookUpApp.swift
@@ -0,0 +1,18 @@
+//
+//  LookUpApp.swift
+//  LookUp
+//  
+//  Created by Andrew Zheng (github.com/aheze) on 4/19/24.
+//  Copyright © 2024 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct LookUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
