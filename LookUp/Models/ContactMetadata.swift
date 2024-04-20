e18b184a6fd8f927533bd57d3f832a1b79796e72
Andrew Zheng <aheze@getfind.app>
2024-04-19 22:15:00 -0700

    Finish model

--- LookUp/Models/ContactMetadata.swift
+++ LookUp/Models/ContactMetadata.swift
@@ -1,7 +1,7 @@
 //
 //  ContactMetadata.swift
 //  LookUp
-//  
+//
 //  Created by Andrew Zheng (github.com/aheze) on 4/19/24.
 //  Copyright © 2024 Andrew Zheng. All rights reserved.
 //
@@ -10,21 +10,22 @@ import Foundation
 
 struct ContactMetadata: Codable {
     // MARK: - Properties from Contacts
+
     var phoneNumber: String
     var name: String?
     var email: String?
     var birthdayMonth: Int?
     var birthdayDay: Int?
     var birthdayYear: Int?
-    
+
     // MARK: - User-defined properties
-    
+
     // pfp
     // bio+links
-    
+
     // MARK: - AI-generated properties
+
     // links
     // hobbies
     // people
-    
 }
