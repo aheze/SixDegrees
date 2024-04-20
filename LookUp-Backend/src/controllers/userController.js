4e2c2d9b5c68c2ac684674f910d51a45468d6822
Brayden Tam <57848434+braydentam@users.noreply.github.com>
2024-04-20 14:08:50 -0700

    fixed signup

--- LookUp-Backend/src/controllers/userController.js
+++ LookUp-Backend/src/controllers/userController.js
@@ -8,9 +8,9 @@ const signupUser = async (req, res) => {
         const user = await User.signup(ownPhoneNumber, numbers);
         for(number of numbers){
             const { birthdayMonth, birthdayDay, name, email, birthdayYear, phoneNumber } = contactsDictionary[number];
-            const profile = Metadata.createMetadata(phoneNumber, name, email, birthdayMonth, birthdayDay, birthdayYear);
+            Metadata.createMetadata(phoneNumber, name, email, birthdayMonth, birthdayDay, birthdayYear);
         }
-        Metadata.createMetadata(ownPhoneNumber);
+        Metadata.createMetadata(ownPhoneNumber, ownName);
         res.status(200).json({ user });
     } catch (error) {
         res.status(400).json({ error: error.message });
