9179261f0a469974b05155d20f6f7b8fc2addf42
Neel Redkar <neel.redkar@gmail.com>
2024-04-20 17:15:49 -0700

    invalid URL handling

--- src/utils/fetchWebsite.ts
+++ src/utils/fetchWebsite.ts
@@ -3,35 +3,40 @@ import puppeteer from "puppeteer";
 import fs from "fs/promises";
 
 export async function fetchWebsite(url: string) {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  const cookiesString = await fs.readFile("./cookies.json");
-  const cookies = JSON.parse(cookiesString.toString());
-  await page.setCookie(...cookies);
+  try {
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    const cookiesString = await fs.readFile("./cookies.json");
+    const cookies = JSON.parse(cookiesString.toString());
+    await page.setCookie(...cookies);
 
-  await page.goto(url, { waitUntil: "load" });
-  await page.goto(url, { waitUntil: "domcontentloaded" });
-  await page.goto(url, { waitUntil: "networkidle0" });
-  await page.goto(url, { waitUntil: "networkidle2" });
+    await page.goto(url, { waitUntil: "load" });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+    await page.goto(url, { waitUntil: "networkidle0" });
+    await page.goto(url, { waitUntil: "networkidle2" });
 
-  const text = await page.evaluate(() => document.body.innerText);
+    const text = await page.evaluate(() => document.body.innerText);
 
-  const allImageLinks = await page.evaluate(() => {
-    const images = Array.from(document.getElementsByTagName("img"));
-    return images.map((image) => image.src);
-  });
+    const allImageLinks = await page.evaluate(() => {
+      const images = Array.from(document.getElementsByTagName("img"));
+      return images.map((image) => image.src);
+    });
 
-  await browser.close();
+    await browser.close();
 
-  // remove image data:image/png;base64
-  const filteredImages = allImageLinks.filter(
-    (link) => !link.includes("data:image/png;base64")
-  );
+    // remove image data:image/png;base64
+    const filteredImages = allImageLinks.filter(
+      (link) => !link.includes("data:image/png;base64")
+    );
 
-  return [text, filteredImages];
+    return [text, filteredImages];
+  } catch (e) {
+    console.error(e);
+    return ["", []];
+  }
 }
 
 /* export async function fetchWebsite(url: string): Promise<string> {
